4cc26984984894c8047f5f69327988d41cfdd486
aikey <office.at@mail.ru>
2023-05-17 13:03:31 +0600

    First commit Message

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,94 @@
 const About = () => {
     return (
         <div className="docPage">
-            <h1>About</h1>
+            <h1>About Us</h1>
             <hr></hr>
+            <div className="newsBox">
+                
+                <div className="aboutGit">
+                    <div>
+                        <img src="https://avatars.githubusercontent.com/u/55422492?v=4" alt="avatar"
+                            className="avatarGit"
+                        ></img>
+                    </div>
+                    <div>
+                        <p></p>
+                        <b>Frontend</b><br></br>
+                        <span>by Aidarkhan Tulegenov</span>
+                    </div>
+                    
+                </div>
+
+                <br></br>
+                <div>
+                    <p>github - <a href="https://github.com/a1key">link</a></p>
+                </div>
+            </div>
+
+            <div className="newsBox">
+                
+                <div className="aboutGit">
+                    <div>
+                        <img src="https://avatars.githubusercontent.com/u/74859821?v=4" alt="avatar"
+                            className="avatarGit"
+                        ></img>
+                    </div>
+                    <div>
+                        <p></p>
+                        <b>Fullstack</b><br></br>
+                        <span>by Dinmukhamed Kossanov</span>
+                    </div>
+                </div>
+
+                <br></br>
+                <div>
+                    <p>github - <a href="https://github.com/Kosn-3006">link</a></p>
+                </div>
+            </div>
+
+            <div className="newsBox">
+                
+                <div className="aboutGit">
+                    <div>
+                        <img src="https://avatars.githubusercontent.com/u/55399621?v=4" alt="avatar"
+                            className="avatarGit"
+                        ></img>
+                    </div>
+                    <div>
+                        <p></p>
+                        <b>Backend</b><br></br>
+                        <span>by Batyrkhan Olzhabay</span>
+                    </div>
+                </div>
+
+                <br></br>
+                <div>
+                    <p>github - <a href="https://github.com/I3Fly">link</a></p>
+                </div>
+            </div>
+
+            <div className="newsBox">
+                
+                <div className="aboutGit">
+                    <div>
+                        <img src="https://avatars.githubusercontent.com/u/131770299?s=96&v=4" alt="avatar"
+                            className="avatarGit"
+                        ></img>
+                    </div>
+                    <div>
+                        <p></p>
+                        <b>Diploma approval system</b><br></br>
+                        <span></span>
+                    </div>
+                </div>
+
+                <br></br>
+                <div>
+                    <p>github repository - <a href="https://github.com/Diploma-makers/Diploma_Approval_System.git">link</a></p>
+                </div>
+            </div>
         </div>
+        
     )
 }
 
